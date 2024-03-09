df4514b510cb206d40d1929dc0c1b09b573c27f6
jmyers0 <jmye0292@students.vsu.edu>
2024-03-08 23:11:51 -0500

    Initial commit

---
+++ app/src/main/java/com/example/codepathmail/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.codepathmail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)}
+
+    }
